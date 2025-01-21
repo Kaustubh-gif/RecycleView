d532168a68ebf9522ae530f22c5a8dfef0a32524
kaustubh <kulkarnikaustubh10@gmail.com>
2025-01-21 16:20:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.recycleview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ProductsAdapter productsAdapter;
+    private ArrayList<Product> productArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initDataSet();
+        RecyclerView productsRecyclerView = findViewById(R.id.productsRecyclerView);
+        productsAdapter = new ProductsAdapter(productArrayList);
+        productsRecyclerView.setAdapter(productsAdapter);
+        productsRecyclerView.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL,
+                false));
+    }
+
+    private void initDataSet(){
+        productArrayList = new ArrayList<Product>();
+        for(int i =0;i<10;i++) {
+            productArrayList.add(new Product(i + 1,
+                    "Product " + i,
+                    i * 5 + 10,
+                    i
+            ));
+        }
+    }
+}
